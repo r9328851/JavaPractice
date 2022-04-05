bf569be09312c5fe64e4da2a17dcd3c5b2f7cde5
r9328851 <66528057+r9328851@users.noreply.github.com>
2022-04-05 18:10:15 +0530

    Update and rename HelloJava.java to Introduction.java

---
+++ Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction{                              // public class
+    public static void main(String[] args) {            // main method
+        System.out.println("Java is fun!");             // function used to print the statements
+        System.out.println("Java -> Python.");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+}
