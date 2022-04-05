b3f5e20724cb2ccc10252c9097041ddcce18b210
r9328851 <66528057+r9328851@users.noreply.github.com>
2022-04-05 18:03:38 +0530

    Create HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava{ // public class
+    public static void main(String[] args) { // main method
+        System.out.println("Java is fun!");   // function used 
+        System.out.println("Java -> Python.");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+    }
+}
