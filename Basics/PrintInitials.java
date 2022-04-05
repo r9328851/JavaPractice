ac82170f3864e40d6c582fae2b0270b02bc0fff5
r9328851 <66528057+r9328851@users.noreply.github.com>
2022-04-05 18:49:19 +0530

    Commit to main

---
+++ Basics/PrintInitials.java
@@ -0,0 +1,12 @@
+public class PrintInitials {
+    public static void main(String[] args) {
+
+        System.out.println("R  R  R  R");
+        System.out.println("R           R");
+        System.out.println("R           R");
+        System.out.println("R  R  R  R");
+        System.out.println("R  R");
+        System.out.println("R      R");
+        System.out.println("R          R");
+    }    
+}
